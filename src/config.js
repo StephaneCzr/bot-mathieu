875e728f6ba5bf1065600e9ac713960d304618fb
StephaneCzr <stephane.czr@gmail.com>
2017-04-11 17:00:15 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 process.env.PORT = '5000'
 process.env.REQUEST_TOKEN = '08c09e2bd44dba9f0aa875f60a8ca085'
 process.env.LANGUAGE = 'en'
-process.env.GITHUB_TOKEN = 'fd72e39837c843fe7a00818d75e471d21bccd9fa'
-process.env.GITHUB_USERNAME = 'MatthieuPeyrot'
+process.env.GITHUB_TOKEN = 'f37d05ece079b92042bd622a7184f0cae4d2b813'
+process.env.GITHUB_USERNAME = 'StephaneCzr'
